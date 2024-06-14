ade43b94347913566e2c4fe04bbcd691a4786b46
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-06-14 23:27:42 +0700

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -43,6 +43,15 @@ function set_empty_inventory(inventory) {
     inventory.Build.Value = false;
 }
 
+// задает в контекст инвентаря пустой инвентарь
+function seet_empty_inventory(inventory) {
+    inventory.Main.Value = false;
+    inventory.Secondary.Value = false;
+    inventory.Melee.Value = false;
+    inventory.Explosive.Value = false;
+    inventory.Build.Value = false;
+}
+
 // задает опции режима мир, выбранные при создании комнаты
 export function apply_room_options() {
     const gameModeParameters = room.GameMode.Parameters;
@@ -78,7 +87,12 @@ export function create_teams() {
     const hasRedTeam = roomParameters.GetBool("RedTeam");
     const hasBlueTeam = roomParameters.GetBool("BlueTeam");
     if (hasRedTeam || !hasRedTeam && !hasBlueTeam) {
-        teams.create_team_red();
+        const redTeam = teams.create_team_red();
+        if (roomParameters.GetBool("RedHasNothing")) {
+            seet_empty_inventory(redTeam.Inventory);
+        }
+    }
+        
     }
     if (hasBlueTeam || !hasRedTeam && !hasBlueTeam) {
         const blueTeam = teams.create_team_blue();
