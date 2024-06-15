5c629014690b8e736a9145bcd513bcf95c6bd2fd
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-06-15 14:06:39 +0700

    Update peace_inventory.js

--- client/room/peace_inventory.js
+++ client/room/peace_inventory.js
@@ -1,6 +1,39 @@
-import { Inventory } from 'pixel_combats/room';
+import { Inventory, GameMode } from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
 // Инвентарь (Обычная версия)
-co
+  if (GameMode.Parameters.GetBool("Inventory")) {
+  const Inventory = room.Inventory.GetContext();
+  Inventory.Main.Value = false;
+  Inventory.Secondary.Value = false;
+  Inventory.Melee.Value = true;
+  Inventory.Explosive.Value = false;
+  Inventory.Build.Value = true;
+  Inventory.BuildInfinity.Value = true;
+ }
+
+// Инвентарь (ТДМ версия)
+   if (GameMode.Parameters.GetBool("InventoryTDM")) {
+    const Inventory = room.Inventory.GeContext();
+    Inventory.Main.Value = true;
+    Inventory.MainInfinity.Value = false;
+    Inventory.Secondary.Value = true;
+    Inventory.SecondaryInfinity.Value = false;
+    Inventory.Melee.Value = true;
+    Inventory.Explosive.Value = true;
+    Inventory.ExplosiveInfinity.Value = false;
+    Inventory.Build.Value = true;
+    Inventory.BuildInfinity.Value = false;
+   }
+
+// Инвентарь (Для красных нету)
+   if (GameMode.Parameters.GetBool("RedInventory")) {
+    const Inventory = room.Inventory.GetContext();
+    Inventory.Main.Value = false;
+    Inventory.Secondary.Value = false;
+    Inventory.Melee.Value = false;
+    Inventory.Explosive.Value = false;
+    Inventory.Build.Value = false;
+   }
+  
 
