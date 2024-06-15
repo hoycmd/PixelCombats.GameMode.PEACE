dc7cfb7480c7db548ab82f4bace6fbf6e81a6ec3
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-06-15 11:59:11 +0700

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -1,5 +1,5 @@
 import { Color } from 'pixel_combats/basic';
-import { Teams } from 'pixel_combats/room';
+import { Teams, LeaderBoard } from 'pixel_combats/room';
 
 export const RED_TEAM_NAME = "Red";
 export const BLUE_TEAM_NAME = "Blue";
@@ -19,3 +19,9 @@ export function create_team_blue() {
     Teams.Get(BLUE_TEAM_NAME).Spawns.SpawnPointsGroups.Add(BLUE_TEAM_SPAWN_POINTS_GROUP);
     return Teams.Get(BLUE_TEAM_NAME);
 }
+
+LeaderBoard.RoomLeaderBoardValues = [
+  new DisplayValueHeader("Kills", "<b>Киллы</b>", "<b>Киллы</b>"),
+  new DisplayValueHeader("Deaths", "<b>Смерти</b>", "<b>Смерти</b>"),
+  new DisplayValueHeader("Scores", "<b>Очки</b>", "<b>Очки</b>"),
+];
