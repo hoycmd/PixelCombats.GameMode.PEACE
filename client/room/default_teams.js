6679b5e39f194b84a3d873be3ef590b480a1e415
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-06-15 12:09:07 +0700

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -1,5 +1,5 @@
 import { Color } from 'pixel_combats/basic';
-import { Teams, LeaderBoard, Player, Ui, Properties, Timer } from 'pixel_combats/room';
+import { Teams, Team } from 'pixel_combats/room';
 
 export const RED_TEAM_NAME = "Red";
 export const BLUE_TEAM_NAME = "Blue";
@@ -20,36 +20,3 @@ export function create_team_blue() {
     return Teams.Get(BLUE_TEAM_NAME);
 }
 
-LeaderBoard.PlayerLeaderBoardValues = [
-  new DisplayValueHeader("Kills", "<b>Киллы</b>", "<b>Киллы</b>"),
-  new DisplayValueHeader("Deaths", "<b>Смерти</b>", "<b>Смерти</b>"),
-  new DisplayValueHeader("Scores", "<b>Очки</b>", "<b>Очки</b>"),
-];
-
-LeaderBoard.PlayersWeightGetter.Set(function(player) {
-  return player.Properties.Get("Scores").Value;
-});
-
-Ui.GetContext().TeamProp1.Value = { Team: "Blue", Prop: "Deaths" };
-Ui.GetContext().TeamProp2.Value = { Team: "Red", Prop: "Deaths" };
-
-var immortalityTimerName = "immortality";
-Spawns.GetContext().OnSpawn.Add(function(player){
-  player.Properties.Immortality.Value = true;
-  timer = player.Timers.Get(immortalityTimerName).Restart(5);
-});
-Timers.OnPlayerTimer.Add(function(timer){
-  if (timer.Id != immortalityTimerName) return;
-  timer.Player.Properties.Immortality.Value = false;
-});
-
-Damage.OnDeath.Add(function(player) {
-  ++player.Properties.Deaths.Value;
-});
-
-Damage.OnKill.Add(function(player, killed) {
-  if (player.id !== killed.id) { 
-    ++player.Properties.Kills.Value;
-    player.Properties.Scores.Value += 100;
-  }
-});
