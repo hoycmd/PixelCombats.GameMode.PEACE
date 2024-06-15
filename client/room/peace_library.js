1ce6c17203e0bbb7424e286c098df4a71fcd82ad
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-06-15 13:13:07 +0700

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -79,7 +79,11 @@ export function create_teams() {
     const hasRedTeam = roomParameters.GetBool("RedTeam");
     const hasBlueTeam = roomParameters.GetBool("BlueTeam");
     if (hasRedTeam || !hasRedTeam && !hasBlueTeam) {
-        teams.create_team_red();
+    const redTeam = teams.create_team_red();
+        if (roomParameters.GetBool("RedHasNothing")) {
+            set_empty_inventory(redTeam.Inventory);
+        }
+    }
     }
     if (hasBlueTeam || !hasRedTeam && !hasBlueTeam) {
         const blueTeam = teams.create_team_blue();
