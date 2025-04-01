3fdc29897531f0f2f7312ed86a0b320cd1a1dfcd
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-01 13:22:18 +0700

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -1,10 +1,3 @@
-import { Color } from 'pixel_combats/basic';
-import { Teams } from 'pixel_combats/room';
+import * as Room from 'pixel_combats/room';
 
-// Создаём, команды
-Teams.Add("Blue", "<b><i>Синие| v1.577 - PC2 API 2.0</i></b>", new Color(0, 0, 1, 0));
-Teams.Add("Red", "<b><i>Красные| v1.577 - PC2 API2.0</i></b>", new Color(1, 0, 0, 0,));
-var BlueTeam = Teams.Get("Blue");
-var RedTeam = Teams.Get("Red");
-BlueTeam.Spawns.SpawnPointsGroups.Add(1);
-RedTeam.Spawns.SpawnPointsGroups.Add(2);
+
