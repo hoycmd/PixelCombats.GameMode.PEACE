7ee7050a6a9d8ee36d938a0dcb9b2009509e4d6d
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-01 13:59:49 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -6,16 +6,13 @@ const ImmortalityTimerName = 'Immortality';
 const MessageBlue = '<b><size=62><color=Green>M</a><color=Red>ᴛ</a><a>ʀ</a></size></b>';
 const MessageRed = '<b><size=82><color=Green>ᴮʸ:</a> <color=Red>ƬNƬ</a><color=Yellow>!</a></size></b>';
 
-const RedTeam = MTR.CreateRedTeam();
-const BlueTeam = MTR.CreateBlueTeam();
-BlueTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
-RedTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
-
 MTR.EditSettings();
 MTR.InventoryAll();
 
+Red = Room.GameMode.Parameters.GetBool('RedTeam');
+Blue = Room.GameMode.Parameters.GetBool('BlueTeam');
 Room.BreackGraph.WeakBlocks = Room.GameMode.Parameters.GetBool('LoosenBlocks');
-Room.BreackGraph.OnlyPlayerBlocksDmg = Room.GameMode.Parameters.GetBool('OnlyPlayerBlocksDmg');
+Room.BreackGraph.OnlyPlayerBlocksDmg = Room.GameMode.Parameters.GetBool('PartialDesruction');
 Room.Damage.GetContext().FriendlyFire = Room.GameMode.Parameters.GetBool('FriendlyFire');
 Room.Damage.GetContext().DamageOut.Value = true;
 Room.BreackGraph.PlayerBlockBoost = true;
@@ -39,3 +36,27 @@ Timers.OnPlayerTimer.Add(function(Timer) {
 	if (Timer.Id != Immortality) return;
 	  Timer.Player.Properties.Immortality.Value = false;
 });
+
+if (Blue || !Red && Blue) { 
+  const BlueTeam = MTR.CreateBlueTeam();
+  BlueTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
+if (Room.GameMode.Parameters.GetBool('BlueHasNothing')) {
+ Room.Teams.Get('BlueTeam').Inventory.Main.Value = false;
+ Room.Teams.Get('BlueTeam').Inventory.Secondary.Value = false;
+ Room.Teams.Get('BlueTeam').Inventory.Melee.Value = false;
+ Room.Teams.Get('BlueTeam').Inventory.Explosive.Value = false;
+ Room.Teams.Get('BlueTeam').Inventory.Build.Value = false;
+    }
+}
+ if (Red || !Red && !Blue) {
+  const RedTeam = MTR.CreateRedTeam();
+  RedTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
+if (Room.GameMode.Parameters.GetBool('RedHasNothing')) {
+ Room.Teams.Get('RedTeam').Inventory.Main.Value = false;
+ Room.Teams.Get('RedTeam').Inventory.Secondary.Value = false;
+ Room.Teams.Get('RedTeam').Inventory.Melee.Value = false;
+ Room.Teams.Get('RedTeam').Inventory.Explosive.Value = false;
+ Room.Teams.Get('RedTeam').Inventory.Build.Value = false;
+     }
+  }
+
