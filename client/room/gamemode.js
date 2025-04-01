1659cef06a56244ec27cc8c2e501578acb08a8ec
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-01 20:30:15 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,7 +1,6 @@
 import * as Room from 'pixel_combats/room';
 import * as Basic from 'pixel_combats/basic';
 import * as TeamMTR from './Teams_TeamMTR.js;';
-import * as EditInvMTR from './Inventory_EditInvMTR.js';
 
 try {
 
@@ -13,8 +12,34 @@ const BlueTeam = TeamMTR.CreateBlueTeam();
 BlueTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
 RedTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
 
-EditInvMTR.EditSettings();
-EditInvMTR.InventoryAll();
+const inventory = Room.Inventory.GetContext();
+ inventory.Main.Value = true; 
+ inventory.MainInfinity.Value = true;
+ inventory.Secondary.Value = true;
+ inventory.SecondaryInfinity.Value = true;
+ inventory.Melee.Value = true;
+ inventory.Explosive.Value = true;
+ inventory.ExplosiveInfinity.Value = true;
+ inventory.Build.Value = true;
+ inventory.BuildInfinity.Value = true;
+
+ Room.Build.GetContext().Pipette.Value = true;
+ Room.Build.GetContext().FloodFill.Value = true;
+ Room.Build.GetContext().FillQuad.Value = true;
+ Room.Build.GetContext().RemoveQuad.Value = true;
+ Room.Build.GetContext().BalkLenChange.Value = true;
+ Room.Build.GetContext().FlyEnable.Value = true;
+ Room.Build.GetContext().SetSkyEnable.Value = true;
+ Room.Build.GetContext().GenMapEnable.Value = true;
+ Room.Build.GetContext().ChangeCameraPointsEnable.Value = true;
+ Room.Build.GetContext().QuadChangeEnable.Value = true;
+ Room.Build.GetContext().BuildModeEnable.Value = true;
+ Room.Build.GetContext().CollapseChangeEnable.Value = true;
+ Room.Build.GetContext().RenameMapEnable.Value = true;
+ Room.Build.GetContext().ChangeMapAuthorsEnable.Value = true;
+ Room.Build.GetContext().LoadMapEnable.Value = true;
+ Room.Build.GetContext().ChangeSpawnsEnable.Value = true;
+ Room.Build.GetContext().BuildRangeEnable.Value = true;
 	
 Room.BreackGraph.WeakBlocks = Room.GameMode.Parameters.GetBool('LoosenBlocks');
 Room.BreackGraph.OnlyPlayerBlocksDmg = Room.GameMode.Parameters.GetBool('PartialDesruction');
