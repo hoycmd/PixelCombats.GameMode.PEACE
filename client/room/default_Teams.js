04492b299080a64e05957db3ce8d42425afdad1b
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-02 16:59:17 +0700

    Update default_Teams.js

--- client/room/default_Teams.js
+++ client/room/default_Teams.js
@@ -1,6 +1,9 @@
+// Библиотека, созданая как для - команд.
+// Импорты:
 import { Teams } from 'pixel_combats/room';
 import { Color } from 'pixel_combats/basic';
 
+// Константы, для названии части - команд:
 export const BlueTeam_NAME = 'Blue';
 export const RedTeam_NAME = 'Red';
 export const BlueTeam_DISPLAYNAME = '<b><size=30><color=#0d177c>ß</color><color=#03088c>l</color><color=#0607b0>ᴜ</color><color=#1621ae>E</color></size></b>';
@@ -10,11 +13,13 @@ export const RedTeam_SPAWNPOINTSGROUP = 2;
 export const BlueTeam_COLOR = new Color(0, 0, 1, 0);
 export const RedTeam_COLOR = new Color(1, 0, 0, 0);
 
+// Функция, создания - синей команды:
 export function CreateBlueTeam() {
  Teams.Add(BlueTeam_NAME, BlueTeam_DISPLAYNAME, BlueTeam_COLOR);
  Spawns.SpawnPointsGroups.Add(BlueTeam_SPAWNPOINTSGROUP);
 }
 
+// Функция, создания - красной команды:
 export function CreateRedTeam() {
  Teams.Add(RedTeam_NAME, RedTeam_DISPLAYNAME, RedTeam_COLOR);
  Spawns.SpawnPointsGroups.Add(RedTeam_SPAWNPOINTSGROUP);
