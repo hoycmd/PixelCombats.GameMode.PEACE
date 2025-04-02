0c83aff5a9b4705847539c40ed4dc01257bca310
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-02 20:18:12 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,31 +1,28 @@
 // Импорты:
 import { Build, Spawns, GameMode, Ui, Teams, BreackGraph, BuildBlocksSet, Damage, Players } from 'pixel_combats/room';
-import * as Teams from './default_teams.js';
-import * as Mtr from './mtr_library.js';
+import * as teams from './default_teams.js';
+import * as mtr from './mtr_library.js';
 
 try {
  
 // Настройки команд:
-const Red = GameMode.Parameters.GetBool('RedTeam'); 
-const Blue = GameMode.Parameters.GetBool('BlueTeam');
- if (Red || !Red && !Blue) = Teams.CreateRedTeam(); 
- if (Blue || !Red && !Blue) = Teams.CreateBlueTeam(); 
-// Задаём, все блоки:
-Build.GetContext().BlocksSet.Value = BuildBlocksSet.All; 
+ const RedTeam = teams.CreateRedTeam(); 
+ const BlueTeam = teams.CreateBlueTeam();
+ RedTeam.Build.BlocksSet.Value = BuildBlocksSet.All;
+ BlueTeam.Build.BlocksSet.Value = BuildBlocksSet.All;
 // Задаём, настройку - для инвентаря:
-Mtr.SetInventory();
+mtr.SetInventory();
 
 // Параметры, при создании - комнаты:
 BreackGraph.WeakBlocks = GameMode.Parameters.GetBool('LoosenBlocks');
 BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool('PartialDesruction');
-Damage.GetContext().FriendlyFire = GameMode.Parameters.GetBool('FriendlyFire');
-Build.GetContext().FlyEnable = GameMode.Parameters.GetBool('Fly');	
+Damage.GetContext().FriendlyFire = GameMode.Parameters.GetBool('FriendlyFire');	
 // Базовые функции, включённые в комнате:
 Damage.GetContext().DamageOut.Value = true;
 BreackGraph.PlayerBlockBoost = true;
 
 // Настройка редактора, в комнате:
-Mtr.SetEditor();
+mtr.SetEditor();
 
 // Вход в команду, по запросу:
 Teams.OnRequestJoinTeam.Add(function(Player, Team) { Team.Add(Player); });
@@ -37,7 +34,3 @@ Ui.GetContext().Hint.Value = 'MTR - By: TnT!';
 
 // Моментальный спавн:
 Spawns.GetContext().RespawnTime.Value = 5;
-
-} catch (e) {
-	Players.All.forEach(msg => msg.Show(`(error.name) ${e.name}: (error.message) ${e.message}, (error.stack) ${e.stack};`));
-}
