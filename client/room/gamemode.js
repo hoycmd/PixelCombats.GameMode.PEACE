88320d0b7b1dbe512cbe169d89d7f420a1eb9e5b
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-02 15:31:55 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,45 +1,16 @@
 import * as Room from 'pixel_combats/room';
 import * as Basic from 'pixel_combats/basic';
-import * as TeamMTR from './Teams_TeamMTR.js;';
+import * as Teams from './default_teams.js;';
 
 try {
 
 const MessageBlue = '<b><size=62><color=Green>M</a><color=Red>ᴛ</a><a>ʀ</a></size></b>';
 const MessageRed = '<b><size=82><color=Green>ᴮʸ:</a> <color=Red>ƬNƬ</a><color=Yellow>!</a></size></b>';
 
-const RedTeam = TeamMTR.CreateRedTeam();
-const BlueTeam = TeamMTR.CreateBlueTeam();
+const RedTeam = Room.Teams.CreateRedTeam();
+const BlueTeam = Room.Teams.CreateBlueTeam();
 BlueTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
 RedTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.All;
-
-const inventory = Room.Inventory.GetContext();
- inventory.Main.Value = true; 
- inventory.MainInfinity.Value = true;
- inventory.Secondary.Value = true;
- inventory.SecondaryInfinity.Value = true;
- inventory.Melee.Value = true;
- inventory.Explosive.Value = true;
- inventory.ExplosiveInfinity.Value = true;
- inventory.Build.Value = true;
- inventory.BuildInfinity.Value = true;
-
- Room.Build.GetContext().Pipette.Value = true;
- Room.Build.GetContext().FloodFill.Value = true;
- Room.Build.GetContext().FillQuad.Value = true;
- Room.Build.GetContext().RemoveQuad.Value = true;
- Room.Build.GetContext().BalkLenChange.Value = true;
- Room.Build.GetContext().FlyEnable.Value = true;
- Room.Build.GetContext().SetSkyEnable.Value = true;
- Room.Build.GetContext().GenMapEnable.Value = true;
- Room.Build.GetContext().ChangeCameraPointsEnable.Value = true;
- Room.Build.GetContext().QuadChangeEnable.Value = true;
- Room.Build.GetContext().BuildModeEnable.Value = true;
- Room.Build.GetContext().CollapseChangeEnable.Value = true;
- Room.Build.GetContext().RenameMapEnable.Value = true;
- Room.Build.GetContext().ChangeMapAuthorsEnable.Value = true;
- Room.Build.GetContext().LoadMapEnable.Value = true;
- Room.Build.GetContext().ChangeSpawnsEnable.Value = true;
- Room.Build.GetContext().BuildRangeEnable.Value = true;
 	
 Room.BreackGraph.WeakBlocks = Room.GameMode.Parameters.GetBool('LoosenBlocks');
 Room.BreackGraph.OnlyPlayerBlocksDmg = Room.GameMode.Parameters.GetBool('PartialDesruction');
