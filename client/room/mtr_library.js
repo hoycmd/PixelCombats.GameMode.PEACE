2258e6d80e025e2cb4506ecf681d5a4fa263f447
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-02 15:47:26 +0700

    Create mtr_library.js

---
+++ client/room/mtr_library.js
@@ -0,0 +1,37 @@
+import * as Room from 'pixel_combats/room';
+
+function SetInventory() {
+  const inventory = Room.Inventory.GetContext();
+   inventory.Main.Value = true; 
+   inventory.MainInfinity.Value = true;
+   inventory.Secondary.Value = true;
+   inventory.SecondaryInfinity.Value = true;
+   inventory.Melee.Value = true;
+   inventory.Explosive.Value = true;
+   inventory.ExplosiveInfinity.Value = true;
+   inventory.Build.Value = true;
+   inventory.BuildInfinity.Value = true;
+}
+
+function SetEditor() {
+ const build = Room.Build.GetContext();
+  build.Pipette.Value = true;
+  build.FloodFill.Value = true;
+  build.FillQuad.Value = true;
+  build.RemoveQuad.Value = true;
+  build.BalkLenChange.Value = true;
+  build.SetSkyEnable.Value = true;
+  build.GenMapEnable.Value = true;
+  build.ChangeCameraPointsEnable.Value = true;
+  build.QuadChangeEnable.Value = true;
+  build.BuildModeEnable.Value = true;
+  build.CollapseChangeEnable.Value = true;
+  build.RenameMapEnable.Value = true;
+  build.ChangeMapAuthorsEnable.Value = true;
+  build.LoadMapEnable.Value = true;
+  build.ChangeSpawnsEnable.Value = true;
+  build.BuildRangeEnable.Value = true;
+}
+
+const Red = Room.GameMode.Parameters.GetBool('RedTeam');
+const Blue = Room.GameMode.Parameters.GetBool('BlueTeam');
