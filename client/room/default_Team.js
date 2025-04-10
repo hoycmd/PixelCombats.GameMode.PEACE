4b5d1cc2cee8360101042969094f89dccd8514e7
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-10 10:34:49 +0700

    Update and rename default_teams.js to default_Team.js

--- client/room/default_Team.js
+++ client/room/default_Team.js
@@ -1,7 +1,7 @@
 // Библиотека, созданая как для - команд.
 // Импорты:
-import { Teams } from 'pixel_combats/room';
-import { Color } from 'pixel_combats/basic';
+import * as Room from 'pixel_combats/room';
+import * as Basic from 'pixel_combats/basic';
 
 // Константы, для названии части - команд:
 export const BlueTeam_NAME = 'Blue';
@@ -10,17 +10,17 @@ export const BlueTeam_DISPLAYNAME = '<b><size=30><color=#0d177c>ß</color><color
 export const RedTeam_DISPLAYNAME = '<b><size=30><color=#962605>尺</color><color=#9a040c>ᴇ</color><color=#b8110b>D</color></size></b>';
 export const BlueTeam_SPAWNPOINTSGROUP = 1;
 export const RedTeam_SPAWNPOINTSGROUP = 2;
-export const BlueTeam_COLOR = new Color(0, 0, 1, 0);
-export const RedTeam_COLOR = new Color(1, 0, 0, 0);
+export const BlueTeam_COLOR = new Basic.Color(0, 0, 1, 0);
+export const RedTeam_COLOR = new Basic.Color(1, 0, 0, 0);
 
 // Функция, создания - синей команды:
 export function CreateBlueTeam() {
- Teams.Add(BlueTeam_NAME, BlueTeam_DISPLAYNAME, BlueTeam_COLOR);
- Spawns.SpawnPointsGroups.Add(BlueTeam_SPAWNPOINTSGROUP);
+ Room.Teams.Add(BlueTeam_NAME, BlueTeam_DISPLAYNAME, BlueTeam_COLOR);
+ Room.Spawns.SpawnPointsGroups.Add(BlueTeam_SPAWNPOINTSGROUP);
 }
 
 // Функция, создания - красной команды:
 export function CreateRedTeam() {
- Teams.Add(RedTeam_NAME, RedTeam_DISPLAYNAME, RedTeam_COLOR);
- Spawns.SpawnPointsGroups.Add(RedTeam_SPAWNPOINTSGROUP);
+ Room.Teams.Add(RedTeam_NAME, RedTeam_DISPLAYNAME, RedTeam_COLOR);
+ Room.Spawns.SpawnPointsGroups.Add(RedTeam_SPAWNPOINTSGROUP);
 }
