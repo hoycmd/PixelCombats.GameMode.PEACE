64cf05023d120b40cc20cd3dfc4779d8d1ca9c32
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-19 00:36:09 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -10,13 +10,29 @@ room.Build.GetContext().FlyEnable = room.GameMode.Parameters.GetBool('Fly');
 room.BreackGraph.OnlyPlayerBlocksDmg = room.GameMode.Parameters.GetBool('PartialDesruction');
 room.Damage.GetContext().FriendlyFire = room.GameMode.Parameters.GetBool('FriendlyFire');
 room.Damage.GetContext().DamageOut.Value = room.GameMode.Parameters.GetBool('DamageOut');
-
-room.BreackGraph.BreackAll = true;
 room.Build.GetContext().BlocksSet.Value = room.BuildBlocksSet.AllClear; if (room.GameMode.Parameters.GetBool('AllSetBlocks')) {
 room.Build.GetContext().BlocksSet.Value = room.BuildBlocksSet.All;
+
+room.BreackGraph.BreackAll = true;
 room.TeamsBalancer.IsAutoBalance = true;
+room.Ui.GetContext().TeamProp1.Value = { Team: 'blue', Prop: 'Text' };  
+room.Teams.Get('blue').Properties.Get('Text').Value = 'Тгк создателя: @TnTPixelCombats2!';
+room.Ui.GetContext().Hint.Value = 'MTR - By: TnT!';
+room.Ui.GetContext().QuadsCount.Value = true;
+room.Spawns.GetContext().RespawnTime.Value = 5;
 
 room.Teams.OnRequestJoinTeam.Add(function(p,t){t.Add(p);});
-room.Teams.
-  
+room.Teams.OnPlayerChangeTeam.Add(function(p){ 
+ for (const i = 0; i <= PlayersBanList.length; i++){
+   if (PlayersBanList[i] === p.id){
+  p.Spawns.Enable = false;
+  p.Spawns.Despawn();
+  p.Team.Remove(p);
+   } else {
+  p.Spawns.Spawn();
+           }
+     }
+}
 
+mtr.set_inventory();
+mtr.set_editor();
