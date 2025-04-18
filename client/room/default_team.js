7991d8f2611915ea4aa774c1a8827914cd1c9062
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-19 00:41:14 +0700

    Update default_team.js

--- client/room/default_team.js
+++ client/room/default_team.js
@@ -1,26 +1,21 @@
-// Библиотека, созданая как для - команд.
-// Импорты:
-import * as Room from 'pixel_combats/room';
-import * as Basic from 'pixel_combats/basic';
+import * as room from 'pixel_combats/room';
+import * as basic from 'pixel_combats/basic';
 
-// Константы, для названии части - команд:
-export const BlueTeam_NAME = 'Blue';
-export const RedTeam_NAME = 'Red';
-export const BlueTeam_DISPLAYNAME = '<b><size=30><color=#0d177c>ß</color><color=#03088c>l</color><color=#0607b0>ᴜ</color><color=#1621ae>E</color></size></b>';
-export const RedTeam_DISPLAYNAME = '<b><size=30><color=#962605>尺</color><color=#9a040c>ᴇ</color><color=#b8110b>D</color></size></b>';
-export const BlueTeam_SPAWNPOINTSGROUP = 1;
-export const RedTeam_SPAWNPOINTSGROUP = 2;
-export const BlueTeam_COLOR = new Basic.Color(0, 0, 1, 0);
-export const RedTeam_COLOR = new Basic.Color(1, 0, 0, 0);
+export const blue_team_name = 'blue';
+export const red_team_name = 'red';
+export const red_team_displayname = '<size=59>尺ᴇD</size>';
+export const blue_team_displayname = '<size=59>ßlᴜE</size>';
+export const blue_team_spawnpointsgroups = '1';
+export const red_team_spawnpointsgroups = '2';
+export const blue_team_newcolorteam = new basic.Color(0, 0, 0.2, 0);
+export const red_team_newcolorteam = new basic.Color(0.2, 0, 0, 0);
 
-// Функция, создания - синей команды:
-export function CreateBlueTeam() {
- Room.Teams.Add(BlueTeam_NAME, BlueTeam_DISPLAYNAME, BlueTeam_COLOR);
- Room.Teams.Get('BlueTeam_NAME').SpawnPointsGroups.Add(BlueTeam_SPAWNPOINTSGROUP);
+export function create_blue_team() {
+ room.Teams.Add(blue_team_name, blue_team_displayname, blue_team_newcolorteam);
+ room.Teams.Get(blue_team_name).SpawnPointsGroups.Add(blue_team_spawnpointsgroups);
 }
 
-// Функция, создания - красной команды:
-export function CreateRedTeam() {
- Room.Teams.Add(RedTeam_NAME, RedTeam_DISPLAYNAME, RedTeam_COLOR);
- Room.Teams.Get('RedTeam_NAME').SpawnPointsGroups.Add(RedTeam_SPAWNPOINTSGROUP);
+export function create_red_team() {
+ room.Teams.Add(red_team_name, red_team_displayname, red_team_newcolorteam);
+ room.Teams.Get(red_team_name).SpawnPointsGroups.Add(red_team_spawnpointsgroups);
 }
