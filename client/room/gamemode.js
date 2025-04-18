819ae7f48cb7d5c47b93ef3db0dcd6eeed55ffd4
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2025-04-19 00:02:23 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,5 +1,5 @@
 import * as room from 'pixel_combats/room';
-import * as basic from 'pixel_combats/basic;
+import * as basic from 'pixel_combats/basic';
 import * as team from './default_team.js';
 import * as mtr from './library_mtr.js';
 
